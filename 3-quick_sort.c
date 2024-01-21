10c0b364bb2a9b9e1d2d0f081e98d9bc4a7609c9
ScottShadow <rudaseswascottmc@gmail.com>
2024-01-21 14:52:54 +0000

    quick sort fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -14,6 +14,18 @@ void swap(int *a, int *b)
 	*a = *b;
 	*b = temp;
 }
+void print_array_range(int *array, size_t start, size_t print_size)
+{
+	size_t i;
+
+	for (i = 0; i < print_size; i++)
+	{
+		printf("%d", array[start + i]);
+		if (i < print_size - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
 /**
  * quick_sort - Sorts an array of integers using the Quick Sort algorithm.
  * @array: Pointer to the array to be sorted.
@@ -22,26 +34,40 @@ void swap(int *a, int *b)
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t pivot, current, temp;
+	size_t pivot;
 
 	if (size <= 1)
 		return;
+	pivot = partiton(array, size);
+
+	if (pivot > 0)
+		quick_sort(array, pivot);
+	if (pivot < size - 1)
+		quick_sort(array + pivot + 1, size - pivot - 1);
+}
+/**
+ * partition - Chooses the last element as the pivot and
+ *             partitions the array accordingly.
+ * @array: Pointer to the array to be partitioned.
+ * @size: Size of the array.
+ * Return: The index of the pivot after partitioning.
+ */
+size_t partiton(int *array, size_t size)
+{
+	size_t pivot, current, temp;
 
 	pivot = size - 1;
 	current = 0;
-	temp = current - 1;
+	temp = 0;
 
-	while (current <= pivot)
+	for (current = 0; current < size - 1; current++)
 	{
-		if (array[current] < array[pivot] || current == pivot)
+		if (array[current] < array[pivot])
 		{
-			temp++;
 			swap(&array[current], &array[temp]);
-			print_array(array, size);
+			temp++;
 		}
-		current++;
 	}
-	if (temp > 0)
-		quick_sort(array, temp);
-	quick_sort(array + temp + 1, size - temp - 1);
+	swap(&array[current], &array[temp]);
+	return (temp);
 }
