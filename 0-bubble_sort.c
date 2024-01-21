46fd0e3dedae8f8117686e2ddcecdccf4763f9ee
ScottShadow <rudaseswascottmc@gmail.com>
2024-01-21 11:37:42 +0000

    removed optimization

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,23 +25,24 @@ void swap(int *a, int *b)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i = 0, j = 0;
-	int swapped;
+	/* int swapped; */
+
 	if (size == 1)
 		return;
 	for (j = size - 1; j >= 1; j--)
 	{
-		swapped = 0;
+		/* swapped = 0; */
 		for (i = 0; i < j; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
 				swap(&array[i], &array[i + 1]);
-				swapped = 1;
+				/* swapped = 1; */
 				print_array(array, size);
 			}
 		}
 
-		if (!swapped)
-			break;
+		/* if (!swapped) */
+		/* 	break; */
 	}
 }
