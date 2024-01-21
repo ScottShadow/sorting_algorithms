8f69d4039132472cce256c7ce586a9929c3ce731
ScottShadow <rudaseswascottmc@gmail.com>
2024-01-21 11:46:40 +0000

    reverted

--- 0-main.c
+++ 0-main.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,7 +9,7 @@
  */
 int main(void)
 {
-    int array[] = {0, 0, 0, 0};
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     print_array(array, n);
