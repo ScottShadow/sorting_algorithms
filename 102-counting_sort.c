7499a74268bced7d0752ecb06a2137e3d3ecabb4
ScottShadow <rudaseswascottmc@gmail.com>
2024-01-24 20:13:34 +0000

    minor fix

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -33,42 +33,42 @@ int findmax(int *array, size_t size)
 void counting_sort(int *array, size_t size)
 {
 	int max = 0;
-	int *count = NULL, *output = NULL;
+	int *count_arr = NULL, *rez_arr = NULL;
 	int i;
 	size_t j;
 
 	if (array == NULL || size == 0)
 		return;
 	max = findmax(array, size);
-	count = malloc((max + 1) * sizeof(int));
-	if (count == NULL)
+	count_arr = malloc((max + 1) * sizeof(int));
+	if (count_arr == NULL)
 		return;
-	output = malloc(size * sizeof(int));
-	if (output == NULL)
+	rez_arr = malloc(size * sizeof(int));
+	if (rez_arr == NULL)
 	{
-		free(count);
+		free(count_arr);
 		return;
 	}
 	for (i = 0; i < max + 1; i++)
-		count[i] = 0;
+		count_arr[i] = 0;
 
 	for (j = 0; j < size; j++)
-		count[array[j]]++;
+		count_arr[array[j]]++;
 	for (i = 1; i <= max; i++)
-		count[i] = count[i - 1] + count[i];
+		count_arr[i] = count_arr[i - 1] + count_arr[i];
 
-	print_array(count, max + 1);
+	print_array(count_arr, max + 1);
 
 	for (j = size - 1; j != SIZE_MAX; j--)
 	{
-		output[count[array[j]] - 1] = array[j];
-		count[array[j]]--;
+		rez_arr[count_arr[array[j]] - 1] = array[j];
+		count_arr[array[j]]--;
 	}
 
 	for (j = 0; j < size; j++)
 	{
-		array[j] = output[j];
+		array[j] = rez_arr[j];
 	}
-	free(count);
-	free(output);
+	free(count_arr);
+	free(rez_arr);
 }
